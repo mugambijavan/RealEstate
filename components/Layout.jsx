91dbe458ebb7fce20fff9702bdab5c88d39761a8
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-11-24 21:06:53 +0000

    Apex Properties

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>RealEstate</title>
+        <title>Apex Properties</title>
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import { FiKey } from 'react-icons/fi';
 const Navbar = () => (
   <Flex p='2' borderBottom='1px' borderColor='gray.100'>
     <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
-      <Link href='/' paddingLeft='2'>RealEstate</Link>
+      <Link href='/' paddingLeft='2'>Apex Properties</Link>
     </Box>
     <Spacer />
     <Box>
